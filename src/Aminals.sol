81e3b8891001465d4eac09c5f2b3e0517dd76c97
Will Papper <contact@papper.me>
2023-06-26 18:52:14 +0200

    Sketched outline of data schema
    Also ran the forge formatter via Prettier on all files

--- src/Aminals.sol
+++ src/Aminals.sol
@@ -1,2 +1,49 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.20;
+
+contract Aminals {
+    mapping(uint256 aminalId => Aminal) public aminals;
+
+    struct Aminal {
+        uint8 totalLove;
+        uint8 energy;
+        mapping(address user => uint8 love) lovePerUser;
+        Visuals visuals;
+        mapping(uint8 => Skills) skills;
+    }
+
+    // TODO: Migrate to VisualsRegistry
+    struct Visuals {
+        string body;
+        string hat;
+        string eyes;
+        string mouth;
+        string nose;
+        string limbs;
+        string tail;
+        string accessories;
+    }
+
+    // TODO: Migrate to SkillsRegistry
+    struct Skills {
+        string name;
+        address contractAddress;
+        // Human-readable ABI format
+        string functionSignature;
+    }
+
+    function feed() public {}
+
+    function breed() public {}
+
+    function squeak() public {}
+
+    function addSkill() public {}
+
+    function addVisuals() public {}
+
+    function callSkill(uint256 aminalId) public {
+        aminals[aminalId].energy -= 1;
+        // Call skill
+    }
+}
--- src/utils/SkillsRegistry.sol
+++ src/utils/SkillsRegistry.sol
@@ -1,2 +1,4 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.20;
+
+// Looks up Skills by ID and converts them to Skills
