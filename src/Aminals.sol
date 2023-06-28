d5773480b94bf76a5293b60b3bda5a4ee9613cad
Will Papper <contact@papper.me>
2023-06-28 16:29:48 +0200

    Added WIP Visuals Auction

--- src/Aminals.sol
+++ src/Aminals.sol
@@ -7,10 +7,6 @@ import "forge-std/Test.sol";
 import "./utils/FeedBondingCurve.sol";
 import "./libs/ABDKMathQuad.sol";
 
-
-
-
-
 contract Aminals {
     mapping(uint256 aminalId => Aminal aminal) public aminals;
 
@@ -18,6 +14,7 @@ contract Aminals {
         uint256 totalLove;
         // TODO: Check whether gas usage is the same for a uint128
         uint256 energy;
+        bool breeding;
         mapping(uint256 aminalTwoId => bool readyToBreed) breedableWith;
         mapping(address user => uint256 love) lovePerUser;
         Visuals visuals;
@@ -26,14 +23,14 @@ contract Aminals {
 
     // TODO: Migrate to VisualsRegistry
     struct Visuals {
-        string body;
-        string hat;
-        string eyes;
-        string mouth;
-        string nose;
-        string limbs;
-        string tail;
-        string accessories;
+        uint256 body;
+        uint256 hat;
+        uint256 eyes;
+        uint256 mouth;
+        uint256 nose;
+        uint256 limbs;
+        uint256 tail;
+        uint256 accessories;
     }
 
     // TODO: Migrate to SkillsRegistry
@@ -45,20 +42,19 @@ contract Aminals {
     }
 
     function getAminalLoveTotal(uint256 aminalID) public view returns (uint256) {
-       Aminal storage aminal = aminals[aminalID];
+        Aminal storage aminal = aminals[aminalID];
         return aminal.totalLove;
     }
 
-    function getAminalLoveByIdByUser(uint256 aminalID, address user) public view returns (uint256)  {
+    function getAminalLoveByIdByUser(uint256 aminalID, address user) public view returns (uint256) {
         Aminal storage aminal = aminals[aminalID];
         return aminal.lovePerUser[user];
     }
 
-    function getEnergy(uint256 aminalID) public view returns (uint256)  {
+    function getEnergy(uint256 aminalID) public view returns (uint256) {
         Aminal storage aminal = aminals[aminalID];
         return aminal.energy;
     }
-
 
     function feed(uint256 aminalId) public payable returns (uint256) {
         require(msg.value >= 0.01 ether, "Not enough ether");
@@ -66,10 +62,9 @@ contract Aminals {
 
         //console.log("TESTESTEST: ", msg.sender);
 
-
         // TODO: Amount of love should be on a bonding curve, not a direct
         // addition
-      //  uint256 amount = (msg.value / 10**16);
+        //  uint256 amount = (msg.value / 10**16);
 
         uint256 amount = msg.value;
 
@@ -83,26 +78,25 @@ contract Aminals {
         // aminal.energy += uint8(amount);
 
         // assuming a simple bonding curve, where d(e) = ETH * (1 - e/100)**2
-       // aminal.energy = aminal.energy + (amount * ((1 - aminal.energy/100) ** 2));
-        
+        // aminal.energy = aminal.energy + (amount * ((1 - aminal.energy/100) ** 2));
+
         // assuming a simple bonding curve, where d(e) = (100 - e)/100
         // bytes16 delta = ABDKMathQuad.div( ABDKMathQuad.sub(ABDKMathQuad.fromInt(100), aminal.energy), ABDKMathQuad.fromInt(100) );
         // aminal.energy = ABDKMathQuad.add(aminal.energy, ABDKMathQuad.mul(delta, ABDKMathQuad.fromUInt(amount)));
 
-        uint256 gap = 10**18 - aminal.energy;
+        uint256 gap = 10 ** 18 - aminal.energy;
 
-        uint256 delta = amount * gap / 10**18; 
+        uint256 delta = amount * gap / 10 ** 18;
         aminal.energy += delta;
 
-        // using the bonding curve system 
-       // FeedBondingCurve feedcurve = new FeedBondingCurve();
-       // aminal.energy = feedcurve.feedBondingCurve(amount, aminal.energy);
+        // using the bonding curve system
+        // FeedBondingCurve feedcurve = new FeedBondingCurve();
+        // aminal.energy = feedcurve.feedBondingCurve(amount, aminal.energy);
 
         // return ABDKMathQuad.toUInt(ABDKMathQuad.mul(ABDKMathQuad.fromUInt(amount), delta));
         //     return ABDKMathQuad.toUInt(delta);
 
         return delta;
-
     }
 
     function breedWith(uint256 aminalIdOne, uint256 aminalIdTwo) public payable {
@@ -121,8 +115,8 @@ contract Aminals {
         // the user has high enough love on Aminal Two to maintain simplicity --
         // a wrapper contract can always do this atomically in one transaction)
         if (aminalTwo.breedableWith[aminalIdOne]) {
-            require(aminalOne.energy >=10 && aminalTwo.energy >=10, "Aminal does not have enough energy to breed");
-            
+            require(aminalOne.energy >= 10 && aminalTwo.energy >= 10, "Aminal does not have enough energy to breed");
+
             // TODO: Initiate voting for traits on the Visual registry. Voting is
             // denominated in the combined love of both Aminal One and Aminal Two
         } else {
@@ -139,17 +133,15 @@ contract Aminals {
         Aminal storage aminal = aminals[aminalId];
 
         require(aminal.lovePerUser[msg.sender] >= 1, "Not enough love");
-        
+
         // ensure that aminal.energy never goes below 0
-        if(aminal.energy >= 1) { aminal.energy--; }
-        
+        if (aminal.energy >= 1) aminal.energy--;
+
         // TODO: Migrate the bool to a constant for convenience
         adjustLove(aminalId, 1, msg.sender, false);
     }
 
     function addSkill() public {}
-
-    function addVisuals() public {}
 
     function callSkill(uint256 aminalId, bytes32 skillId, bytes32 data) public payable {
         squeak((aminalId));
@@ -159,7 +151,6 @@ contract Aminals {
 
     // TODO: Switch to passing the Aminal struct instead of the aminalId
     function adjustLove(uint256 aminalId, uint256 love, address sender, bool increment) internal {
-        
         Aminal storage aminal = aminals[aminalId];
         if (!increment) {
             aminal.lovePerUser[sender] -= love;
@@ -173,36 +164,34 @@ contract Aminals {
     // TODO: Add delegation to other addresses. This will likely end up wrapping
     // msg.sender functionality in a library that checks for delegation
 
-
-
-   function log2(uint x) private returns (uint y){
-   assembly {
-        let arg := x
-        x := sub(x,1)
-        x := or(x, div(x, 0x02))
-        x := or(x, div(x, 0x04))
-        x := or(x, div(x, 0x10))
-        x := or(x, div(x, 0x100))
-        x := or(x, div(x, 0x10000))
-        x := or(x, div(x, 0x100000000))
-        x := or(x, div(x, 0x10000000000000000))
-        x := or(x, div(x, 0x100000000000000000000000000000000))
-        x := add(x, 1)
-        let m := mload(0x40)
-        mstore(m,           0xf8f9cbfae6cc78fbefe7cdc3a1793dfcf4f0e8bbd8cec470b6a28a7a5a3e1efd)
-        mstore(add(m,0x20), 0xf5ecf1b3e9debc68e1d9cfabc5997135bfb7a7a3938b7b606b5b4b3f2f1f0ffe)
-        mstore(add(m,0x40), 0xf6e4ed9ff2d6b458eadcdf97bd91692de2d4da8fd2d0ac50c6ae9a8272523616)
-        mstore(add(m,0x60), 0xc8c0b887b0a8a4489c948c7f847c6125746c645c544c444038302820181008ff)
-        mstore(add(m,0x80), 0xf7cae577eec2a03cf3bad76fb589591debb2dd67e0aa9834bea6925f6a4a2e0e)
-        mstore(add(m,0xa0), 0xe39ed557db96902cd38ed14fad815115c786af479b7e83247363534337271707)
-        mstore(add(m,0xc0), 0xc976c13bb96e881cb166a933a55e490d9d56952b8d4e801485467d2362422606)
-        mstore(add(m,0xe0), 0x753a6d1b65325d0c552a4d1345224105391a310b29122104190a110309020100)
-        mstore(0x40, add(m, 0x100))
-        let magic := 0x818283848586878898a8b8c8d8e8f929395969799a9b9d9e9faaeb6bedeeff
-        let shift := 0x100000000000000000000000000000000000000000000000000000000000000
-        let a := div(mul(x, magic), shift)
-        y := div(mload(add(m,sub(255,a))), shift)
-        y := add(y, mul(256, gt(arg, 0x8000000000000000000000000000000000000000000000000000000000000000)))
-    }  
-}
+    function log2(uint256 x) private returns (uint256 y) {
+        assembly {
+            let arg := x
+            x := sub(x, 1)
+            x := or(x, div(x, 0x02))
+            x := or(x, div(x, 0x04))
+            x := or(x, div(x, 0x10))
+            x := or(x, div(x, 0x100))
+            x := or(x, div(x, 0x10000))
+            x := or(x, div(x, 0x100000000))
+            x := or(x, div(x, 0x10000000000000000))
+            x := or(x, div(x, 0x100000000000000000000000000000000))
+            x := add(x, 1)
+            let m := mload(0x40)
+            mstore(m, 0xf8f9cbfae6cc78fbefe7cdc3a1793dfcf4f0e8bbd8cec470b6a28a7a5a3e1efd)
+            mstore(add(m, 0x20), 0xf5ecf1b3e9debc68e1d9cfabc5997135bfb7a7a3938b7b606b5b4b3f2f1f0ffe)
+            mstore(add(m, 0x40), 0xf6e4ed9ff2d6b458eadcdf97bd91692de2d4da8fd2d0ac50c6ae9a8272523616)
+            mstore(add(m, 0x60), 0xc8c0b887b0a8a4489c948c7f847c6125746c645c544c444038302820181008ff)
+            mstore(add(m, 0x80), 0xf7cae577eec2a03cf3bad76fb589591debb2dd67e0aa9834bea6925f6a4a2e0e)
+            mstore(add(m, 0xa0), 0xe39ed557db96902cd38ed14fad815115c786af479b7e83247363534337271707)
+            mstore(add(m, 0xc0), 0xc976c13bb96e881cb166a933a55e490d9d56952b8d4e801485467d2362422606)
+            mstore(add(m, 0xe0), 0x753a6d1b65325d0c552a4d1345224105391a310b29122104190a110309020100)
+            mstore(0x40, add(m, 0x100))
+            let magic := 0x818283848586878898a8b8c8d8e8f929395969799a9b9d9e9faaeb6bedeeff
+            let shift := 0x100000000000000000000000000000000000000000000000000000000000000
+            let a := div(mul(x, magic), shift)
+            y := div(mload(add(m, sub(255, a))), shift)
+            y := add(y, mul(256, gt(arg, 0x8000000000000000000000000000000000000000000000000000000000000000)))
+        }
+    }
 }
--- src/utils/VisualsAuction.sol
+++ src/utils/VisualsAuction.sol
@@ -1,2 +1,33 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.20;
+
+contract VisualsAuction {
+    // TODO: Maintain an index of pending Aminals based on breeding in progress
+    mapping(uint256 auctionId => Auction auction) public auctions;
+
+    struct Auction {
+        uint256 aminalIdOne;
+        uint256 aminalIdTwo;
+        // Max of 10 trait options can be proposed per Aminal. 2 slots are used
+        // by the Aminals' current trains, and the other 8 slots are used by
+        // proposed traits.
+        uint256[8][10] visualIdVotes;
+        // If 0, auction has not yet concluded
+        // The auctionId will be used as the child Aminal's ID
+        uint256 childAminalId;
+    }
+
+    function startAuction(uint256 aminalIdOne, uint256 aminalIdTwo) public returns (bytes32) {
+        // Set the breeding flag on each Aminal
+        // Loop through each current visual per Aminal and write it to the
+        // struct. i.e. Index [N][0] is AminalIdOne's traits, Index [N][1] is
+        // AminalIdTwo's traits.
+        // Then loop through all of Aminal One's proposed traits and write them to the
+    }
+
+    function proposeVisual(uint256 auctionId, uint256 visualId) public {}
+
+    function vote(bytes32 auctionId, uint256 visualId, uint256 aminalId) public {}
+
+    function endAuction(bytes32 auctionId) public {}
+}
