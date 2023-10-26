8a71d5b94fde515a45b45e1171f058591d915e4c
Will Papper <contact@papper.me>
2023-10-27 00:48:21 +0200

    Auto-format in attempt to fix linter

--- script/FeedAminal.s.sol
+++ script/FeedAminal.s.sol
@@ -11,10 +11,13 @@ contract FeedAminal is Script {
         vm.startBroadcast(deployerPrivateKey);
 
         Aminals aminals = Aminals(0xC5cA00b528daA14036F6edeFA0665D6a4201dDd0);
-        
+
         for (uint256 i = 1; i <= 2; i++) {
-            console.log("Aminal love by ID by user: ", aminals.getAminalLoveByIdByUser(i, 0x1f028f240A90414211425bFa38eB4917Cb32c39C));
-            console.log("Aminal love total : ",  aminals.getAminalLoveTotal(i));
+            console.log(
+                "Aminal love by ID by user: ",
+                aminals.getAminalLoveByIdByUser(i, 0x1f028f240A90414211425bFa38eB4917Cb32c39C)
+            );
+            console.log("Aminal love total : ", aminals.getAminalLoveTotal(i));
             // aminals.feed{value: 0.01 ether}(i);
         }
 
