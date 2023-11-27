bcaeea2e5896b8de2c60d86045d45a0829253e3a
Jake Hartnell <no@no-reply.no>
2023-11-27 08:08:24 -0800

    Update aminals list query

--- frontend/src/resources/aminals.ts
+++ frontend/src/resources/aminals.ts
@@ -3,13 +3,14 @@ import { Aminal, AminalsListDocument, execute } from '../../.graphclient';
 
 const BASE_KEY = 'aminals';
 
-export const useAminals = () => {
+export const useAminals = (userAddress: string) => {
   return useQuery<Aminal[]>({
     queryKey: [BASE_KEY],
     queryFn: async () => {
       const response = await execute(AminalsListDocument, {
         first: 10,
         skip: 0,
+        address: userAddress,
       });
       if (response.errors) throw new Error(response.errors[0].message);
       return response.data.aminals;
