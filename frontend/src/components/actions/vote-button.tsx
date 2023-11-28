4630d3aef75f03262e0f049f19e9a3c2a7d47f61
yaoe <pdefilippi@gmail.com>
2023-11-28 04:32:47 +0100

    last fixes

--- frontend/src/components/actions/vote-button.tsx
+++ frontend/src/components/actions/vote-button.tsx
@@ -4,7 +4,7 @@ import { useAccount, useContractWrite, useNetwork } from 'wagmi';
 import contract from '../../../deployments/VisualsAuction.json';
 
 const contractConfig = {
-  address: '0xbdF4BE45f35Fd6a1BABF16c7C38c1403a569E5B0' as Address,
+  address: '0xb83Aa15dbe5636c656571DDbb74257a81f994B87' as Address,
   abi: contract.abi as Abi,
 };
 
