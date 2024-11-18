deefd89220a0c2a0ed917b2415c848dff0ba02e9
0xfff <0xfff@protonmail.com>
2024-11-18 22:21:36 +0100

    chore: remove comments

--- frontend/wagmi.config.ts
+++ frontend/wagmi.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from '@wagmi/cli';
-import { react } from '@wagmi/cli/plugins';
 
 const ABI = {
   Aminals: require('./deployments/sepolia/Aminals.json'),
@@ -19,27 +18,5 @@ export default defineConfig({
       name: 'VisualsAuction',
       address: '0x645b0f55268ef561176f3247d06d0b7742f79819',
     },
-  ],
-  plugins: [
-    // hardhat({
-    //   project: '../contracts',
-    //   deployments: {
-    //     VisualsAuction: {
-    //       //   [hardhatChain.id]: HardhatDeployments.VisualsAuction.address,
-    //       [sepolia.id]: '0x',
-    //       [holesky.id]: '0x645b0f55268ef561176f3247d06d0b7742f79819',
-    //     },
-    //     Aminals: {
-    //       //   [hardhatChain.id]: HardhatDeployments.Aminals.address,
-    //       [sepolia.id]: '0x24BEd8962601Caa39e51F02bdC0251Ae51FF0d70',
-    //       [holesky.id]: '0x81ed8e0325b17a266b2af225570679cfd635d0bb',
-    //     },
-    //   },
-    // }),
-    react({
-      // getHookName({ contractName, type }) {
-      //   return `use${contractName}__${type}`
-      // },
-    }),
   ],
 });
