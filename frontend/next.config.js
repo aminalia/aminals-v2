b55b6af405f1731024b909f3b92c6691c95a0e59
Jake Hartnell <meow@no-reply.com>
2025-07-09 01:56:19 +0100

    Configure static build output

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  distDir: 'out',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
 
